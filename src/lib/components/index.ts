5ff283fd3f253a850471dccf74592e244fa6287a
Aman Singh <amansinghs7010@gmail.com>
2024-11-23 16:51:36 -0400

    added faq section

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,3 +2,5 @@ export { default as HeroSection } from '$lib/components/HeroSection.svelte';
 export { default as Button } from '$lib/components/Button.svelte';
 export { default as ChapterPreview } from '$lib/components/ChapterPreview.svelte';
 export { default as AuthorSection } from '$lib/components/AuthorSection.svelte';
+export { default as FaqSection } from '$lib/components/FaqSection.svelte';
+export { default as FaqItem } from '$lib/components/FaqItem.svelte';
