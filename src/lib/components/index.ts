50b1eb29e3937ce8093c39bc63886c1a5babf036
Aman Singh <amansinghs7010@gmail.com>
2024-11-23 15:57:11 -0400

    added author section

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export { default as HeroSection } from '$lib/components/HeroSection.svelte';
 export { default as Button } from '$lib/components/Button.svelte';
 export { default as ChapterPreview } from '$lib/components/ChapterPreview.svelte';
+export { default as AuthorSection } from '$lib/components/AuthorSection.svelte';
