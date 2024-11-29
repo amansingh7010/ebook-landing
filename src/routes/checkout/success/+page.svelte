afe37181ba27d936cd7c166499818f0db5ad5e80
Aman Singh <amansinghs7010@gmail.com>
2024-11-28 20:15:44 -0400

    Added success and error pages

---
+++ src/routes/checkout/success/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { HeroSection } from '$components';
+</script>
+
+<HeroSection>
+	<h1>THANK YOU FOR</h1>
+	<h1 class="mb-l">PURCHASING</h1>
+	<p class="light-grey mb-s">
+		You've made an excellent choice by selecting this guide to assist you in your journey to Canada.
+		We're confident that the tips and advice in this ebook will help you save time, money, and
+		unnecessary stress as you plan your move.
+	</p>
+	<p class="light-grey mb-s">
+		An email with your ebook is on its way to your inbox, along with a separate purchase
+		confirmation. If you don't see them in a few minutes, please check your spam or promotions
+		folder just in case.
+	</p>
+	<p class="light-grey">
+		We're here to help you every step of the way. If you have any questions or need further support,
+		feel free to contact us. Happy reading, and best of luck with your move!
+	</p>
+</HeroSection>
